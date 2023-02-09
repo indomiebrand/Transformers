54c5b36a6a19217c239363f8574296072a81807b
limzian <S10242376@connect.np.edu.sg>
2023-02-09 16:03:18 +0800

    attempt api number 1

---
+++ script.js
@@ -0,0 +1,54 @@
+
+$(document).ready(function () {
+    const APIKEY = "63e48c27478852088da67f04";
+    $("#update-signup-container").hide();
+    $("#add-update-msg").hide();
+  
+    $("#signup-submit").on("click", function (e) {
+      e.preventDefault();
+
+      let signupEmail = $("#signup-email").val();
+      let signupStudentid = $("#signup-studentid").val();
+      let signupPassword = $("#signup-password").val();
+
+
+      let jsondata = {
+        "email": signupEmail,
+        "studentid" : signupStudentid,
+        "password" : signupPassword
+      };
+  
+      console.log(jsondata);
+
+      let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://autobuttsrollout-174c.restdb.io/rest/signup",
+        "method": "POST", 
+        "headers": {
+          "content-type": "application/json",
+          "x-apikey": APIKEY,
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(jsondata),
+        "beforeSend": function(){
+         
+          $("#signup-submit").prop( "disabled", true);
+        
+          $("#add-signup-form").trigger("reset");
+        }
+      }
+  
+    
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        
+        $("#signup-submit").prop( "disabled", false);
+        
+   
+        $("#add-update-msg").show().fadeOut(3000);
+  
+      });
+    });
+})
